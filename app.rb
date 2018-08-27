23cb8fd679e6de0f911f4dff19ee6f32ff057ccc
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-27 21:47:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,12 @@ require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
   get '/' do
-     erb :user_input
-   end
-     post '/piglatinize' do
-      obj = PigLatinizer.new
-     @piglatinized_text = obj.piglatinize(params[:user_phrase])
-     erb :output
-   end
+    erb:user_input
   end
+  
+  post '/piglatinize' do
+    obj = PigLatinizer.new 
+    @piglatinized_text = obj.piglatinize(params[:user_phrase])
+    erb:output
+  end
+end
